6479cd96aa18681563281220f2c0682c0cbb7183
ryabovd <ryabovd@outlook.com>
2023-08-22 17:22:40 +0700

    Исправить функции mult и main Fixes #5

--- multa.py
+++ multa.py
@@ -6,24 +6,37 @@ import random
 
 def input_number(text):
     ''' input number for number tasks or start number or finish number'''
+    number = input(text).strip()
     while True:
-        number = input(text)
         if check_number(number) == True:
-            return int(number)
+            return number
         else:
-            print(f"{text} не является числом. \nВведите новое число \n")       
+            print(f"{number} не является числом. \nВведите новое число \n")
+            number = input(text).strip()       
 
+def isNumeric(s):
+    return s.isdigit()
+
+
+def is_int(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+        
 
 def check_number(str):
     '''Проверить число или нет'''
-    if str.isnumeric() == True:
+    try:
+        int(str)
         return True
-    else:
+    except ValueError:
         return False
+    
 
-
-def greeting(name, start, finish):
-    print(f'{name}, решаем примеры от {start} до {finish} \n')
+def greeting(name, finish):
+    print(f'{name}, решаем примеры до {finish} \n')
 
 
 def min_max(a, b):
@@ -31,26 +44,35 @@ def min_max(a, b):
     max_number = max(a, b)
     return min_number, max_number
 
+
 def mult(a, b):
     return a * b
+
 
 def name():
     n = input("Как вас зовут? ")
     return n
 
 
+def pair(finish):
+    a = int(random.randint(1, finish))
+    b = int(random.randint(1, 10))    
+    return a, b
+
+
 def main():
     user = name()
-    tries = input_number('Введи кол-во задач: ')
-    start = input_number('Введи ОТ какого числа будут начинатся примеры: ')
-    finish = input_number('Введи ДО каким числом будут заканчиваться примеры: ')
-    start, finish = min_max(start, finish)
-    greeting(user, start, finish)
+    tries = int(input_number('Введи кол-во задач: '))
+#    start = input_number('Введи ОТ какого числа будут начинатся примеры: ')
+    finish = int(input_number('Введи ДО какого числа будут примеры: '))
+#    start, finish = min_max(start, finish)
+    greeting(user, finish)
     countyes = 0
     countno = 0
     for i in range(tries):
-        a = int(random.randint(start,finish))
-        b = int(random.randint(start,finish))
+        a, b = pair(finish)
+#        a = int(random.randint(start,finish))
+#        b = int(random.randint(start,finish))
         print(a, 'X', b, '= ',end=' ')
         answer = int(input())
         correct = mult(a, b)
